3f8b90c37d209869160891933cef10fd82b75172
oskam <oska.magdalena@gmail.com>
2018-03-02 22:52:44 +0100

    small fixes

--- src/game/Cards.js
+++ src/game/Cards.js
@@ -1,42 +1,42 @@
 import _ from 'lodash'
 
-export const TYPE_SPELL = 'TYPE_SPELL'
-export const TYPE_COMMON_SUPPORTER = 'TYPE_COMMON_SUPPORTER'
-export const TYPE_SPECIAL_SUPPORTER = 'TYPE_SPECIAL_SUPPORTER'
-export const TYPE_SECRET = 'TYPE_SECRET' //maybe will be implemented later
+export const TYPE_SPELL = 'TYPE_SPELL';
+export const TYPE_COMMON_SUPPORTER = 'TYPE_COMMON_SUPPORTER';
+export const TYPE_SPECIAL_SUPPORTER = 'TYPE_SPECIAL_SUPPORTER';
+export const TYPE_SECRET = 'TYPE_SECRET'; //maybe will be implemented later
 
 //Card states
-export const STATE_IN_DECK = 'STATE_IN_DECK'
-export const STATE_IN_HAND = 'STATE_IN_HAND'
-export const STATE_ON_TABLE = 'STATE_ON_TABLE'
-export const STATE_END = 'STATE_END'
+export const STATE_IN_DECK = 'STATE_IN_DECK';
+export const STATE_IN_HAND = 'STATE_IN_HAND';
+export const STATE_ON_TABLE = 'STATE_ON_TABLE';
+export const STATE_END = 'STATE_END';
 
 //special abilities names
-export const ONE_TIME_SHIELD = 'oneTimeShield'
-export const MODIFIE_STATS = 'modifieStats'
-export const GIVE_EFFECT = 'giveEffect'
-export const MULTIPLAY_ATTACK = 'multiplayAttack'
+export const ONE_TIME_SHIELD = 'oneTimeShield';
+export const MODIFY_STATS = 'modifyStats';
+export const GIVE_EFFECT = 'giveEffect';
+export const ATTACK_TWICE = 'attackTwice';
 
 //modifie stats types
-export const INCREASE = 'increase'
-export const SET = 'set'
+export const INCREASE = 'increase';
+export const SET = 'set';
 
 //target
-export const SELECTED_ALLIE_SUPPORTER = 'SELECTED_ALLIE_SUPPORTER'
-export const SELECTED_ENEMY_SUPPORTER = 'SELECTED_ENEMY_SUPPORTER'
-export const ALL_ALLIE_SUPPORTERS = 'ALL_ALLIE_SUPPORTERS'
-export const ALL_IN_HAND_SUPPORTERS = 'ALL_IN_HAND_SUPPORTERS'
-export const ALL_ENEMY_SUPPORTERS = 'ALL_ENEMY_SUPPORTERS'
-export const MY_HERO = 'MY_HERO'
-export const ENEMY_HERO = 'ENEMY_HERO'
+export const SELECTED_ALLY_SUPPORTER = 'SELECTED_ALLY_SUPPORTER';
+export const SELECTED_ENEMY_SUPPORTER = 'SELECTED_ENEMY_SUPPORTER';
+export const ALL_ALLIE_SUPPORTERS = 'ALL_ALLIE_SUPPORTERS';
+export const ALL_IN_HAND_SUPPORTERS = 'ALL_IN_HAND_SUPPORTERS';
+export const ALL_ENEMY_SUPPORTERS = 'ALL_ENEMY_SUPPORTERS';
+export const MY_HERO = 'MY_HERO';
+export const ENEMY_HERO = 'ENEMY_HERO';
 
 //put places
-export const PLACE_TABLE = 'PLACE_TABLE'
-export const PLACE_MY_SUPPORTER = 'PLACE_MY_SUPPORTER'
-export const PLACE_ENEMY_SUPPORTET = 'PLACE_ENEMY_SUPPORTET'
+export const PLACE_TABLE = 'PLACE_TABLE';
+export const PLACE_MY_SUPPORTER = 'PLACE_MY_SUPPORTER';
+export const PLACE_ENEMY_SUPPORTET = 'PLACE_ENEMY_SUPPORTET';
 
 
-const commonSupproters = {
+const commonSupporters = {
     murlocScout : {
         type: TYPE_COMMON_SUPPORTER,
         name: 'Murloc Scout',
@@ -84,11 +84,12 @@ const commonSupproters = {
         attack: 4,
         cost: 4,
         imgUrl: 'https://media-hearth.cursecdn.com/avatars/334/821/397.png',
-        infoLink: 'https://www.hearthpwn.com/cards/397-brewmaster'
+        infoLink: 'https://www.hearthpwn.com/cards/397-brewmaster',
+        effects: []
     }
-}
+};
 
-const specialSupproters = {
+const specialSupporters = {
     scarletCrusader : {
         type: TYPE_SPECIAL_SUPPORTER,
         name: 'Scarlet Crusader',
@@ -109,14 +110,14 @@ const specialSupproters = {
         infoLink: 'https://www.hearthpwn.com/cards/184-nightblade',
         appearEffect: {
             target: ENEMY_HERO,
-            type: MODIFIE_STATS,
-            specyfication: {
+            type: MODIFY_STATS,
+            specification: {
                 type: INCREASE,
                 hp: -3
             }
         }
     }
-}
+};
 
 const spells = {
     sealofChampions : {
@@ -127,9 +128,9 @@ const spells = {
         imgUrl: 'https://media-hearth.cursecdn.com/avatars/328/22/22373.png',
         infoLink: 'https://www.hearthpwn.com/cards/22373-seal-of-champions',
         special: {
-            target: SELECTED_ALLIE_SUPPORTER,
+            target: SELECTED_ALLY_SUPPORTER,
             type: GIVE_EFFECT,
-            specyfication: {
+            specification: {
                 effect: ONE_TIME_SHIELD
             }
         }
@@ -143,8 +144,8 @@ const spells = {
         infoLink: 'https://www.hearthpwn.com/cards/49676-smugglers-run',
         special: {
             target: ALL_IN_HAND_SUPPORTERS,
-            type: MODIFIE_STATS,
-            specyfication: {
+            type: MODIFY_STATS,
+            specification: {
                 type: INCREASE,
                 hp: 1,
                 attack: 1
@@ -160,14 +161,14 @@ const spells = {
         infoLink: 'https://www.hearthpwn.com/cards/56-arcane-explosion',
         special: {
             target: ALL_ENEMY_SUPPORTERS,
-            type: MODIFIE_STATS,
-            specyfication: {
+            type: MODIFY_STATS,
+            specification: {
                 type: INCREASE,
                 hp: -1
             }
         }
     }
-}
+};
 
 const deck = {
   murlocScout: 2,
@@ -180,21 +181,21 @@ const deck = {
   sealofChampions: 2,
   sugglerRun: 2,
   arcaneExplosion: 2
-}
+};
 
-export const getAllCards = () => ({...commonSupproters, ...specialSupproters, ...spells})
-export const getSpells = () => ({...spells})
-export const getSupporters = () => ({...commonSupproters})
-export const getSpecialSupproters = () => ({...specialSupproters})
+export const getAllCards = () => ({...commonSupporters, ...specialSupporters, ...spells});
+export const getSpells = () => ({...spells});
+export const getSupporters = () => ({...commonSupporters});
+export const getSpecialSupporters = () => ({...specialSupporters});
 
-var idIterator = 1
+var idIterator = 1;
 
 export const getDeck = (player = {}) => {
-  let result = []
-  let availableCards = getAllCards()
+  let result = [];
+  let availableCards = getAllCards();
   _.forOwn(deck , (v, key) => {
     for(let i = 0; i < v; i++) result.push({...availableCards[key], id: idIterator++, ownerName: player.name, state: STATE_IN_DECK, attackReady: false})
-  })
+  });
 
   return _.shuffle(result)
-}
+};
