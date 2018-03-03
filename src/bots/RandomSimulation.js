9d787dbdae3dc681120d39ca6b6cbd13b0c394c7
oskam <oska.magdalena@gmail.com>
2018-03-03 15:01:22 +0100

    check for game over before playing next move

--- src/bots/RandomSimulation.js
+++ src/bots/RandomSimulation.js
@@ -1,13 +1,13 @@
 import {getPossibleMoves} from "./Simulation";
 import * as cards from "../game/Cards";
 
-class RandomSimulation {
+export default class RandomSimulation {
     constructor(player) {
         this.player = player;
     }
 
     getNextMove(game) {
-        let moves = getPossibleMoves(game.getCurrentPlayer(), game.getNotCurrentPlayer());
+        let moves = getPossibleMoves(this.player, game.getNotCurrentPlayer());
         return {
             onTable: moves.onTable[Math.floor(Math.random()*moves.onTable.length)] || null,
             attack: moves.attacks[Math.floor(Math.random()*moves.attacks.length)] || null
@@ -18,15 +18,16 @@ class RandomSimulation {
         if(game.getCurrentPlayer() !== this.player) return;
 
         let playMove = (move) => {
+            game.playCard(move.source, move.target);
             if(move.target === cards.PLACE_TABLE) {
                 console.log("Player " + this.player.name + " places " + move.source.name + " on table");
             } else {
                 console.log("Player " + this.player.name + " attacks " + move.target.name + " with " + move.source.name);
             }
-            game.playCard(move.source, move.target);
         };
 
         let playMoves = (possibleMoves) => {
+            if (game.isGameOver()) return;
             if(possibleMoves.onTable !== null) {
                 playMove(possibleMoves.onTable);
                 playMoves(this.getNextMove(game));
@@ -40,5 +41,3 @@ class RandomSimulation {
         game.changePlayerTurn();
     }
 }
-
-export default RandomSimulation
