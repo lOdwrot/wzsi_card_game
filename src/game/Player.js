b074e0492cd9db47c49a35911c21cf401b91fb80
oskam <oska.magdalena@gmail.com>
2018-03-03 15:00:58 +0100

    add getStats() method for player and universal cardsStats() function; connect aggressive player simulation type, update switch to use constants not strings

--- src/game/Player.js
+++ src/game/Player.js
@@ -1,6 +1,20 @@
 import Hero from "./Hero";
-import RandomSimulation from "../bots/RandomSimulation";
-import {getVisualizedGameInstance} from "./Table";
+import RandomSimulation from '../bots/RandomSimulation'
+import AggressiveSimulation from '../bots/AggressiveSimulation'
+
+export const MANUAL_PLAYER = 'manual';
+export const RANDOM_PLAYER = 'random';
+export const AGGRESSIVE_PLAYER = 'aggressive';
+
+let cardsStats = (cards) => {
+    let hp = 0, attack = 0, count = 0;
+    cards.forEach(card => {
+        hp += card.hp;
+        attack += card.attack;
+        count++;
+    });
+    return { hp: hp, attack: attack, count: count };
+};
 
 class Player {
     constructor(name, type) {
@@ -12,24 +26,36 @@ class Player {
 
     getSimulation() {
         switch(this.type) {
-            case 'manual':
+            case MANUAL_PLAYER:
                 return null;
-            case 'random':
+            case RANDOM_PLAYER:
                 return new RandomSimulation(this);
+            case AGGRESSIVE_PLAYER:
+                return new AggressiveSimulation(this);
             default:
                 console.error("No such player type: " + this.type);
                 return null;
         }
     }
 
-    playTurn() {
+    playTurn(game) {
         if(this.simulation !== null) {
-            this.simulation.playTurn(getVisualizedGameInstance());
+            this.simulation.playTurn(game);
         }
     }
 
     initTurn() {
         this.hero.initTurn();
+    }
+
+    getStats() {
+        return {
+            table: cardsStats(this.hero.tableCards),
+            hero: {
+                hp: this.hero.hp,
+                attack: this.hero.attack,
+            }
+        }
     }
 }
 
